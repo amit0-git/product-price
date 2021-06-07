08d2be954b1aae44bd1752291cd2c4be80f6e6db
Amit Verma <ak2244859@gmail.com>
2021-06-07 09:23:21 +0530

    port commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect, jsonify, f
 from flask.helpers import flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+import os
 
 
 
@@ -174,4 +175,5 @@ def product_check():
 
 
 if __name__ == "__main__":
-    app.run( debug=True)
+    port=int(os.environ.get('PORT',5000))
+    app.run(host="0.0.0.0",port=port)
